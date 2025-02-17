3e3810c606bc161678413dff20a3f2e8e899bbc8
ildar_safarov <ildarsafarov555@gmail.com>
2025-02-17 19:05:03 +0300

    Финальный штрих

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,10 @@
-import pytest
+import sys
+import os
+
+# Принудительно добавляем `src` в PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+
+# Теперь можно импортировать модули из `src`
 from main import Product, Category
 
 
